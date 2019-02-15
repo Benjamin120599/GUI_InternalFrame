d62c7a2c0eef24d988b2fd90240ce82095ed6fd2
Benjamin Viramontes <benja120599@gmail.com>
2019-02-14 21:21:50 -0600

    Se añadio Masters

--- src/InternalFrame.java
+++ src/InternalFrame.java
@@ -4,6 +4,10 @@ import java.awt.*;
 
 class VentanaPrincipal extends JFrame {
 	
+	JMenuBar menuBar;
+	JMenu masters, booking, testPerform, printing, transaction, labReports, settings, utilities, window, help;
+	
+	
 	public VentanaPrincipal() {
 		
 		getContentPane().setLayout(null);
@@ -12,6 +16,11 @@ class VentanaPrincipal extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
+		
+		menuBar = new JMenuBar();
+		
+		masters = new JMenu("Masters");
+		menuBar.add(masters);
 		
 		
 	}
